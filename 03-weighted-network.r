6c6223a592b0306573043fd7628b327da459dff1
François Briatte <briatte@gmail.com>
2021-04-14 15:11:09 +0200

    variable names and cosmetics

--- 03-weighted-network.r
+++ 03-weighted-network.r
@@ -4,7 +4,7 @@ library(igraph)
 library(tnet)
 
 a <- readr::read_tsv("data/affiliations.tsv", col_types = "ccc") %>%
-  select(abstract, affils = affiliation_clean)
+  select(abstract, affiliation = affiliation_clean)
 
 # export
 readr::write_tsv(a, "data/edges-2mode.tsv")
@@ -12,16 +12,16 @@ readr::write_tsv(a, "data/edges-2mode.tsv")
 ## add an unique ID from 1 to n for each unique affiliation involved
 
 e <- a %>%
-  distinct(abstract, affils, .keep_all = TRUE) %>% # truly needed?
+  distinct(abstract, affiliation, .keep_all = TRUE) %>% # truly needed?
   # numeric IDs for abstracts
   group_by(abstract) %>%
-  mutate(artid = cur_group_id()) %>%
+  mutate(abs_id = cur_group_id()) %>%
   # numeric IDs for affiliations
-  group_by(affils) %>%
+  group_by(affiliation) %>%
   mutate(affils_id = cur_group_id()) %>%
   ungroup()
 
-net <- select(e, e = affils_id, p = artid)
+net <- select(e, e = affils_id, p = abs_id)
 
 # -- trying to do everything above with saved edge list from script 02 ---------
 
@@ -51,14 +51,14 @@ dim(projecting_tm(net, method = "Newman"))
 
 # I think that's because your version (correctly) uses all abstract-affil edges,
 # including when it's from a single author:
-select(e, affils) %>% group_by(affils) %>% count(sort = TRUE)
+select(e, affiliation) %>% group_by(affiliation) %>% count(sort = TRUE)
 
 # mine has e.g. Yale one less time than you because, I guess, there must be a
 # single-authored paper from a Yale person, and that edge is not in edges.tsv
 select(e2, affiliation) %>% group_by(affiliation) %>% count(sort = TRUE)
 
 # this seems to confirms the "I guess" above, abstract 82 = single-authored Yale
-filter(e, artid %in% c(1L, 31L, 38L, 79L, 80L, 82L, 84L, 135L))
+filter(e, abs_id %in% c(1L, 31L, 38L, 79L, 80L, 82L, 84L, 135L))
 
 # ------------------------------------------------------------------------------
 
@@ -71,21 +71,21 @@ onemode <- projecting_tm(net, method = "Newman")
 
 # transform the result into an igraph object
 
-g <- graph_from_data_frame(cbind(onemode$i,onemode$j),directed = FALSE)
+g <- graph_from_data_frame(cbind(onemode$i, onemode$j), directed = FALSE)
 E(g)$weight <- as.double(onemode$w)
 
 # remove multiple lines
-g <- simplify(g, remove.multiple = TRUE, edge.attr.comb = c(weight = "first", type = "ignore"))
+g <- igraph::simplify(g, remove.multiple = TRUE, edge.attr.comb = c(weight = "first", type = "ignore"))
 
-V(g)$name <- as.character(e$affils[match(V(g)$name,e$affils_id)]) # allows to replace numeric id_numbers by affiliations to name vertices
+V(g)$name <- as.character(e$affiliation[ match(V(g)$name, e$affils_id) ]) # allows to replace numeric id_numbers by affiliations to name vertices
 V(g)$label <- V(g)$name # If necessary add "str_to_title"
 
 sum(E(g)$weight) # in case, you use the Netscity method, it would give you the number of articles (66!)
 # with the Newman method, the edges' sum gives you 80.5
 
-flows <- bind_cols(get.edgelist(g),W_collab = E(g)$weight) %>%
-  rename(Affil_i = "...1", Affil_j = "...2", )
+flows <- bind_cols(get.edgelist(g), weight = E(g)$weight) %>%
+  rename(i = "...1", j = "...2", )
 
 # export the flow table
 
-write_tsv(flows, "flows.tsv")
+write_tsv(flows, "data/flows.tsv")
