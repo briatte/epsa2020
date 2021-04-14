82a87651b02a7a53b02c5f5ce1675ae93bab9309
François Briatte <briatte@gmail.com>
2021-04-14 15:00:43 +0200

    unstash

--- 03-weighted-network.r
+++ 03-weighted-network.r
@@ -4,25 +4,43 @@ library(igraph)
 library(tnet)
 
 a <- readr::read_tsv("data/affiliations.tsv", col_types = "ccc") %>%
-  select(abstract, affiliation = affiliation_clean)
+  select(abstract, affils = affiliation_clean)
 
 # export
 readr::write_tsv(a, "data/edges-2mode.tsv")
 
-# build two-mode edge list
+## add an unique ID from 1 to n for each unique affiliation involved
+
 e <- a %>%
-  # remove duplicate (abstract, affiliation) pairs
-  # [NOTE] `.keep_all = TRUE` not required (?), passed for safety (?)
-  distinct(abstract, affiliation, .keep_all = TRUE) %>%
+  distinct(abstract, affils, .keep_all = TRUE) %>% # truly needed?
   # numeric IDs for abstracts
   group_by(abstract) %>%
-  mutate(abs_id = cur_group_id()) %>%
+  mutate(artid = cur_group_id()) %>%
   # numeric IDs for affiliations
-  group_by(affiliation) %>%
-  mutate(aff_id = cur_group_id()) %>%
+  group_by(affils) %>%
+  mutate(affils_id = cur_group_id()) %>%
   ungroup()
 
-<<<<<<< Updated upstream
+net <- select(e, e = affils_id, p = artid)
+
+# -- trying to do everything above with saved edge list from script 02 ---------
+
+# one-mode edge list
+e2 <- read_tsv("data/edges-1mode.tsv", col_types = "ccci")
+# two-mode edge list
+e2 <- bind_rows(
+  select(e2, abstract, affiliation = i),
+  select(e2, abstract, affiliation = j)
+) %>%
+  # get numeric id for abstracts (event)
+  group_by(abstract) %>%
+  mutate(abs_id = cur_group_id(), .before = 2) %>%
+  # get numeric id for affiliations (actor)
+  group_by(affiliation) %>%
+  mutate(aff_id = cur_group_id()) %>%
+  distinct() %>%
+  ungroup()
+
 # e2 and net are different in size...
 nrow(net)
 nrow(e2)
@@ -63,56 +81,11 @@ V(g)$name <- as.character(e$affils[match(V(g)$name,e$affils_id)]) # allows to re
 V(g)$label <- V(g)$name # If necessary add "str_to_title"
 
 sum(E(g)$weight) # in case, you use the Netscity method, it would give you the number of articles (66!)
-                 # with the Newman method, the edges' sum gives you 80.5
+# with the Newman method, the edges' sum gives you 80.5
 
 flows <- bind_cols(get.edgelist(g),W_collab = E(g)$weight) %>%
-         rename(Affil_i = "...1", Affil_j = "...2", )
+  rename(Affil_i = "...1", Affil_j = "...2", )
 
 # export the flow table
 
 write_tsv(flows, "flows.tsv")
-=======
-# (affiliation -> abstract) two-mode network
-n <- select(e, aff_id, abs_id) %>%
-  as.matrix() %>%
-  # weighting with Newman's method
-  projecting_tm(method = "Newman")
-
-# [NOTE] Alternative weighting method: Netscity
-#
-# See Whole Normalised Counting method used in [1,2]
-# [1] https://framagit.org/MarionMai/netscityr
-# [2] https://www.irit.fr/netscity
-#
-# Function: `projecting_tm_twisted(net, method = "Netscity")`
-# URL: https://framagit.org/MarionMai/netscityr/-/blob/master/Twisted-Projecting_tm-Function.R
-
-# igraph object
-g <- igraph::graph_from_data_frame(cbind(n$i, n$j), directed = FALSE)
-E(g)$weight <- n$w
-
-# remove multiple edges and loops
-g <- igraph::simplify(
-  g,
-  remove.multiple = TRUE,
-  edge.attr.comb = c(weight = "first", type = "ignore")
-)
-
-# [TOFIX] `match` produces NA values
-V(g)$name <- e$affiliation[ match(V(g)$name, e$aff_id) ]
-V(g)$label <- V(g)$name # if necessary, add `str_to_title`
-
-# with the Newman method, the edges' sum gives you 80.5
-# in case, you use the Netscity method, it would give you the number of articles (66!)
-sum(E(g)$weight)
-
-# export flow table
-bind_cols(
-  get.edgelist(g),
-  W_collab = E(g)$weight
-) %>%
-  rename(Affil_i = "...1", Affil_j = "...2") %>%
-  readr::write_tsv("data/flows.tsv")
->>>>>>> Stashed changes
-
-# have a nice day
