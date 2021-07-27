e78ad19b9a31bac89528c6c850b53dc1529dcfb7
François Briatte <briatte@gmail.com>
2021-07-27 18:53:28 +0200

    Create make.r

---
+++ make.r
@@ -0,0 +1,12 @@
+# download and preprocess
+source("01-data.r")
+
+# example graphs
+source("02-unweighted-network.r")
+source("03-weighted-network.r")
+source("04-weighted-network-plot.r")
+
+# extract full program information
+source("05-full-program.r")
+
+# kthxbye
